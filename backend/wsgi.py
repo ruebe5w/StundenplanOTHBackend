49506fad9b2eaea54860a8b4dab1a5565de1618c
ruebe5w <ruebe@mailbox.org>
2024-09-07 09:29:30 +0200

    Change wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-from django.contrib.auth.handlers.modwsgi import check_password
-from django.core.handlers.wsgi import WSGIHandler
+#from django.contrib.auth.handlers.modwsgi import check_password
+#from django.core.handlers.wsgi import WSGIHandler
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
